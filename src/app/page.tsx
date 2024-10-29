305ab11c5754474a427cd845763c7d679ba86157
Joel Vargas Reynoso <76189923+joelvshimself@users.noreply.github.com>
2024-10-28 23:19:23 -0600

    Titulo Bonito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,15 @@ import { BackgroundGradientAnimation } from '../components/ui/background-gradien
 
 export default function MainPage() {
   return (
+    
     <>
-      Hola
-      <BackgroundGradientAnimation/>
+      <BackgroundGradientAnimation>
+      <div id ="HeroTitle"  style={{fontSize: 'clamp(5rem, 15vw, 15rem)',lineHeight: '1',}} className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold pointer-events-none text-center">
+        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+          STDC
+        </p>
+      </div>
+    </BackgroundGradientAnimation>
     </>
   );
 }
